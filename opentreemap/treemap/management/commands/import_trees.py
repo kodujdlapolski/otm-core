6c589799eed271271ce354a67747b0f241d977f3
sireliah <sajuukk@gmail.com>
2017-03-13 22:12:50 +0100

    Saving the tree instance.

--- opentreemap/treemap/management/commands/import_trees.py
+++ opentreemap/treemap/management/commands/import_trees.py
@@ -3,6 +3,7 @@
 import os
 import json
 
+from django.db import transaction
 from django.conf import settings
 from django.contrib.gis.geos.geometry import Point
 from django.core.management.base import BaseCommand
@@ -12,30 +13,9 @@ from treemap.instance import Instance
 from treemap.models import Tree, Plot, User, Species
 from treemap.lib.geo_tools import ProjectionConverter
 
-def parse_species_name(species_input):
-    ''' "Picea abies 'Viminalis'" '''
-    ''' [Picea, abies, Viminalis ]'''
-    ''' "Crataegus x media 'Paul's Scarlet'" '''
-    '''[Crataegus, x media, Paul's Scarlet] '''
-    genus = None
-    species = None
-    cultivar = None
-    gatunek_kultywar = species_input.split('\'')
-    gatunek = gatunek_kultywar[0]    
-    print gatunek
-    if len(gatunek_kultywar) >=2:
-        kultywar = gatunek_kultywar[1]
-    gatunek_split = gatunek.split()
-    genus = gatunek_split[0]
-    if len(gatunek_split)>=2:
-        if gatunek_split[1] =='x':
-            gatunek_split[1]=gatunek_split[1]+' '+gatunek_split[2]
-        species = gatunek_split[1]    
-    return [genus,species,cultivar]
-
 
 class Command(BaseCommand):
-    
+
     """
     Import trees form file.
     """
@@ -49,16 +29,37 @@ class Command(BaseCommand):
         else:
             self.stdout.write(self.style.ERROR('No such file!'))
 
-    def get_or_create_species(self, properties, instance):
+    def parse_species_name(self, species_input):
+        ''' "Picea abies 'Viminalis'" '''
+        ''' :return: "Picea", "abies", "Viminalis"'''
+
+        genus = None
+        species = None
+        cultivar = None
+        species_cultivar = species_input.split('\'')
+        gatunek = species_cultivar[0]
+
+        if len(species_cultivar) >= 2:
+            cultivar = species_cultivar[1]
+        gatunek_split = gatunek.split()
+        genus = gatunek_split[0]
+
+        if len(gatunek_split) >= 2:
+            if gatunek_split[1] == 'x':
+                gatunek_split[1] = gatunek_split[1] + ' ' + gatunek_split[2]
+            species = gatunek_split[1]
+        return genus, species, cultivar
+
+    def get_or_create_species(self, properties, instance, admin):
         species = Species.objects.filter(common_name=properties['gatunek'], instance=instance)
         if not species.exists():
-            # TODO additional logic here
-            [genus,species,cultivar] = parse_species_name(properties['gatunek_1'])
-            specie = Species(common_name=properties['gatunek'],
-                             genus = genus,
-                             species = species,
-                             cultivar = cultivar)
-#            raise
+            genus, species, cultivar = self.parse_species_name(properties['gatunek_1'])
+            species = Species(common_name=properties['gatunek'],
+                              genus=genus,
+                              species=species,
+                              instance=instance)
+            species.cultivar = cultivar or ''
+            species.save_with_user(admin)
         else:
             return species.last()
 
@@ -95,40 +96,27 @@ class Command(BaseCommand):
             trees = j['features']
 
             for tree in trees:
+                with transaction.atomic():
 
-                lon_wgs84, lat_wgs84 = tree['geometry']['coordinates']
-                lon_webm, lat_webm = converter.to_webmercator(lon_wgs84, lat_wgs84)
-                plot_geom = Point(lon_webm, lat_webm, srid=3857)
+                    lon_wgs84, lat_wgs84 = tree['geometry']['coordinates']
+                    lon_webm, lat_webm = converter.to_webmercator(lon_wgs84, lat_wgs84)
+                    plot_geom = Point(lon_webm, lat_webm, srid=3857)
 
-                plot = self.get_or_create_plot(tree, instance, plot_geom, admin)
-                species = self.get_or_create_species(tree['properties'], instance)
-                
-                # TODO;
-                # wysokosc  = float(properties['wysokosc'].replace(',','.'))
-                # if wysokosc == 0.0:
-                #     wysokosc = 0.1
-                # 
-                # diameter =  float(properties['srednica_k'].replace(',','.'))
-                # 
-                # if diameter  ==  0.0:
-                # 
-                # #        obwod  =  float(properties['pnie_obwod'].replace(',','.'))
-                # 
-                # #        diameter = obwod/math.pi
-                # 
-                # #        if diameter  ==  0.0:
-                # 
-                #         diameter = 0.1
-                # 
-                # new_tree = Tree(
-                #     plot=new_plot,
-                #     diameter=diameter,
-                #     readonly=False,
-                #     canopy_height=wysokosc,
-                #     instance=instance,
-                #     height=wysokosc,
-                #     species=specie
-                # )
-                # 
-                # new_tree.save_with_user(admin)
+                    plot = self.get_or_create_plot(tree, instance, plot_geom, admin)
+                    species = self.get_or_create_species(tree['properties'], instance, admin)
+                    height = float(tree['properties']['wysokosc'].replace(',','.'))
+                    diameter = float(tree['properties']['srednica_k'].replace(',','.'))
 
+                    tree = Tree.objects.filter(plot=plot, instance=instance, species=species)
+
+                    if not tree.exists():
+                        new_tree = Tree(
+                             plot=plot,
+                             diameter=diameter,
+                             readonly=False,
+                             canopy_height=height,
+                             instance=instance,
+                             height=height,
+                             species=species
+                        )
+                        new_tree.save_with_user(admin)
