6fd31c8383523a74e432ccd8c9f16fe99eb88076
sireliah <sajuukk@gmail.com>
2019-05-26 01:13:26 +0200

    Use user postgres for running the app locally.

--- scripts/sql/new_extension.sql
+++ scripts/sql/new_extension.sql
@@ -1,9 +1,7 @@
-CREATE USER otm_kdp ENCRYPTED PASSWORD 'postgres';
-
 \c template1
 CREATE EXTENSION IF NOT EXISTS hstore;
 \c template1
 CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 \c template1
 CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE DATABASE otm_kdp OWNER otm_kdp;
+CREATE DATABASE otm_kdp OWNER postgres;
